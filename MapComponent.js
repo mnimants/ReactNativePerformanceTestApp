3f3ec3ee03ee75ae55293ec9e38cd26b11f00b3e
mnimants <nimantsm@gmail.com>
2018-05-07 16:21:08 +0300

    map view done

--- MapComponent.js
+++ MapComponent.js
@@ -1,67 +1,89 @@
 import React, { Component } from 'react';
 import { Text, View, Button } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { AnimatedRegion, Animated } from 'react-native-maps';
 
 const universityLocation = {
   latitude: 56.9497151,
   longitude: 24.1145698,
-  latitudeDelta: 0.0045,
-  longitudeDelta: 0.0045,
+  latitudeDelta: 0.045,
+  longitudeDelta: 0.045,
 };
 
 const nycLocation = {
   latitude: 40.6976637,
   longitude: -74.1197639,
-  latitudeDelta: 0.0045,
-  longitudeDelta: 0.0045,
+  latitudeDelta: 0.045,
+  longitudeDelta: 0.045,
 };
 
 const londonLocation = {
   latitude: 51.5287718,
   longitude: -0.2416803,
-  latitudeDelta: 0.0045,
-  longitudeDelta: 0.0045,
+  latitudeDelta: 0.045,
+  longitudeDelta: 0.045,
 };
 
 const tokyoLocation = {
   latitude: 35.6735408,
   longitude: 139.5703047,
-  latitudeDelta: 0.0045,
-  longitudeDelta: 0.0045,
+  latitudeDelta: 0.045,
+  longitudeDelta: 0.045,
 };
+
+const animationDuration = 1000;
+const intervalBetweenJumps = 3000;
 
 const locations = [universityLocation, nycLocation, londonLocation, tokyoLocation];
 
 export default class MapComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { locationIndex: 0 };
+    this.intervalId = null;
+    this.locationIndex = 0;
   }
 
-  startJumping = () => {
-    console.log("Console: " + this.state.locationIndex);
+  toggleJumping = () => {
+    if(this.intervalId === null) {
+      this.intervalId = setInterval(() => {
+        this.jump();
+      }, intervalBetweenJumps);
 
-    let nextIndex = this.state.locationIndex + 1;
+      return;
+    }
+
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  };
+
+  jump = () => {
+    console.log("Console: " + this.locationIndex);
+
+    let nextIndex = this.locationIndex + 1;
 
     if(nextIndex == locations.length) {
       nextIndex = 0;
     }
 
-    this.setState({ locationIndex: nextIndex });
-  };
+    if (this.mapView) {
+      this.mapView.animateToCoordinate(locations[nextIndex], animationDuration)
+    }
+
+    this.locationIndex = nextIndex;
+  }
 
   render() {
     return (
       <View style={{ flex: 1 }}>
         <MapView
+          ref={c => this.mapView = c}
           style={{
             flex: 1
           }}
-          region={locations[this.state.locationIndex]}
+          initialRegion={locations[0]}
           />
           <Button
-            onPress={this.startJumping}
-            title="Start jumping"
+            onPress={this.toggleJumping}
+            title="Toggle jumping"
             color="#841584"
             />
       </View>
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 AppRegistry.registerComponent('ReactApp', () => App);
+
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
